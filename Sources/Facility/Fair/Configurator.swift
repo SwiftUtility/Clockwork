b4c5569d74f81ee9c4a07fe3f5497870b7310206
Владимир Бородько <BorodkoVS@msk.bcs.ru>
2022-07-18 14:14:35 +0300

    Switch to trigger token

--- Sources/Facility/Fair/Configurator.swift
+++ Sources/Facility/Fair/Configurator.swift
@@ -102,9 +102,14 @@ public final class Configurator {
         job: GitlabCi.getCurrentJob(verbose: verbose, env: env)
           .map(execute)
           .reduce(Json.GitlabJob.self, jsonDecoder.decode(success:reply:)),
-        apiToken: GitlabCi.makeApiToken(env: env, yaml: yaml)
+        triggerToken: GitlabCi
+          .makeTriggerToken(env: env, yaml: yaml)
           .reduce(env, parse(env:secret:)),
-        pushToken: GitlabCi.makePushToken(env: env, yaml: yaml)
+        apiToken: GitlabCi
+          .makeApiToken(env: env, yaml: yaml)
+          .reduce(env, parse(env:secret:)),
+        pushToken: GitlabCi
+          .makePushToken(env: env, yaml: yaml)
           .reduce(env, parse(env:secret:))
       )
     }
