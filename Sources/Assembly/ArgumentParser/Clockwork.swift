85680260d250cc5540db07fde9325ce4ef85db5d
Vladimir Borodko <vladimirborodko@gmail.com>
2022-07-20 02:47:20 +0300

    Fix trigger api (#10)

--- Sources/Assembly/ArgumentParser/Clockwork.swift
+++ Sources/Assembly/ArgumentParser/Clockwork.swift
@@ -4,7 +4,7 @@ import Facility
 import FacilityPure
 import InteractivityCommon
 struct Clockwork: ParsableCommand {
-  static var version: String { "0.2.0" }
+  static var version: String { "0.2.1" }
   @Option(help: "The path to the profile")
   var profile = ".clockwork.yml"
   @Flag(help: "Should log subprocesses")
