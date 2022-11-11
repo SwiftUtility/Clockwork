73352ca4687a97103c80a8d116168311fe41ad49
Владимир Бородько <BorodkoVS@tech.bcs.ru>
2022-11-11 22:38:12 +0300

    w

--- Sources/Facility/Fair/Reviewer.swift
+++ Sources/Facility/Fair/Reviewer.swift
@@ -348,7 +348,15 @@ public final class Reviewer {
     let fusion = try resolveFusion(.init(cfg: cfg))
     let ctx = try resolveReviewContext(cfg: cfg)
     guard ctx.isActual else { return false }
-    return try changeQueue(cfg: cfg, state: ctx.review, fusion: fusion, enqueue: false)
+    guard try changeQueue(cfg: cfg, state: ctx.review, fusion: fusion, enqueue: false).not
+    else { return true }
+    logMessage(.init(message: "Triggering new pipeline"))
+    try cfg.gitlabCi
+      .flatReduce(curry: ctx.review.iid, GitlabCi.postMrPipelines(review:))
+      .map(execute)
+      .map(Execute.checkStatus(reply:))
+      .get()
+    return false
   }
   public func ownReview(cfg: Configuration) throws -> Bool {
     let fusion = try resolveFusion(.init(cfg: cfg))
@@ -975,11 +983,8 @@ public final class Reviewer {
     for notifiable in notifiables {
       try Execute.checkStatus(reply: execute(gitlab.postMrPipelines(review: notifiable).get()))
     }
-    if enqueue {
-      return queue.isFirst(review: state.iid, target: state.targetBranch)
-    } else {
-      return result
-    }
+    guard enqueue else { return result }
+    return queue.isFirst(review: state.iid, target: state.targetBranch)
   }
   func syncReview(
     cfg: Configuration,
