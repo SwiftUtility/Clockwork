f68a3f457a88531b6cf3f227feea72345ed4dafc
Vladimir Borodko <vladimirborodko@gmail.com>
2023-03-01 01:24:23 +0300

    Fix custom report (#37)

--- Sources/Assembly/ArgumentParser/Clockwork.swift
+++ Sources/Assembly/ArgumentParser/Clockwork.swift
@@ -1,6 +1,6 @@
 import ArgumentParser
 struct Clockwork: ParsableCommand {
-  static var version: String { "0.5.7" }
+  static var version: String { "0.5.8" }
   static let configuration = CommandConfiguration(
     abstract: "Distributed scalable monorepo management tool",
     version: Clockwork.version,
