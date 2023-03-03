961bbc4a0efd4bb93ec1bc429d0ab8090ef302e8
Владимир Бородько <BorodkoVS@tech.bcs.ru>
2023-03-03 11:42:30 +0300

    w

--- Sources/Assembly/ArgumentParser/Clockwork.swift
+++ Sources/Assembly/ArgumentParser/Clockwork.swift
@@ -1,6 +1,6 @@
 import ArgumentParser
 struct Clockwork: ParsableCommand {
-  static var version: String { "0.5.10" }
+  static var version: String { "0.5.11" }
   static let configuration = CommandConfiguration(
     abstract: "Distributed scalable monorepo management tool",
     version: Clockwork.version,
